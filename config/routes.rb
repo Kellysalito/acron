0b0d0115787dbb73de2e058fcdb48d2806549cc2
Kresmlin <kycmmas@gmail.com>
2019-04-15 06:15:04 +0000

    adding about to footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  get 'about', to: 'static_pages#about'
   resources :words, only: [:index, :show]
   namespace :user do
   resources :words, only: [:new, :create, :show]
